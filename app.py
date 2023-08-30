20440da26f31bb59d915376914b9f8a864670ee9
“tford-dev” <terranceford513@gmail.com>
2023-08-30 06:04:58 -0400

    Did some refactoring and editing functions for json file. More refactoring is next and will be making this more user friendly. Also will add notes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import json;
 from utils import database;
-
 
 USER_CHOICE = """
 Enter: 
@@ -12,58 +12,97 @@ Your choice: """
 
 def prompt_add_book():
     book_name = input("Howdy, what is the name of the book you would like to add to the database? ");
-    book_author = input(f"What is the author of the book {book_name}??? ");
-    obj_to_push = {'name': book_name, 'author': book_author, 'read': False}
-    database.books.append(obj_to_push);
+    book_author = input(f"What is the author of the book {book_name.title()}??? ");
+    obj_to_push = {'name': book_name, 'author': book_author, 'read': False};
+    with open('utils/books.json', 'r') as file:
+        current_data = json.load(file);
+    current_data.append(obj_to_push);
+    with open('utils/books.json', 'w') as file:
+        json.dump(current_data, file, indent=4);
     print(f"{obj_to_push} has been added to database.");
 
 def list_books():
-    if(len(database.book) > 0):
-        for book in database.books:
-            print(f"Book Title: {book.name.title()}\n{book.name.title()} Author: {book.author.title()}\n{book.name.title()} read?:");
+    with open('utils/books.json', 'r') as file:
+        current_data = json.load(file);
+    if(len(current_data) > 0):
+        print("Generating list of books in directory...................................................................")
+        for book in current_data:
+            print(f"\nBook Title: {book['name'].title()}\n{book['name'].title()} Author: {book['author'].title()}\n{book['name'].title()} Read?: {book['read']}\n");
     else: 
         print("Directory is empty.");
 
 def prompt_read_book():
     book_name_lookup = input("What is the name of the book you would like to mark as read? ");
-    temporary_obj = None;
-    for book in database.books:
-        if book['name'].lower() == book_name_lookup.lower():
-            database.books.remove(book);
-            temporary_obj = book;
+    with open('utils/books.json', 'r') as file:
+        data = json.load(file);
+    book_match = False;
+    for book in data:
+        if book_name_lookup.lower() == book['name'].lower():
+            book_match = True;
+            if book['read']:
+                print(f"{book['name'].title()} has already been read.");
+            else:
+                book['read'] = True;
+                with open('utils/books.json', 'w') as file:
+                    json.dump(data, file, indent=4)
+                print(f"{book['name'].title()} has been marked as read.");
             break;
-    if temporary_obj == None:
-        print("Book not found in directory. I'm so sorry. ");
-    elif temporary_obj['read'] == True:
-        print(f"{temporary_obj['name'].title()} has already been read.");
-    else:
-        temporary_obj['read'] = True
-        database.books.append(temporary_obj);
-        print(f"{temporary_obj} has been updated and added to database.");
-        print(f"The book {temporary_obj['name'].title()} is now set to read.");
+    if not book_match:
+        print(f"{book_name_lookup.title()} is not in directory.");
+
+""" def prompt_read_book():
+    book_name_lookup = input("What is the name of the book you would like to mark as read? ");
+    with open('utils/books.json', 'r') as file:
+        data = json.load(file);
+    book_match = False;
+    for book in data:
+        if ((book_name_lookup.lower() == book['name'].lower()) and book['read']):
+            print(f"{book['name'].title()} has already been marked as read.");
+            break
+        elif ((book_name_lookup.lower() == book['name'].lower()) and not book['read']):
+            book['read'] = True;
+            with open('utils/books.json', 'w') as file:
+                json.dump(data, file, indent=4);
+            print(f"{book['name'].title()} has been marked as read.");
+            break;
+        else:
+            print(f"{book_name_lookup} is not in directory"); 
+            break; """
 
 def prompt_delete_book():
-    book_name_lookup = input("What is the name of the book you would like to mark as read?");
-    for book in database.books:
-        if book['name'].lower() == book_name_lookup.lower():
-            database.books.remove(book);
-            print(f"{book['name'].title()} has been deleted. ")
-            break;
-    
+    book_name_lookup = input("What is the name of the book you would like to delete? ");
+    with open('utils/books.json', 'r') as file:
+        data = json.load(file);
+    updated_data = [];
+    book_found = False;
+    for book in data:
+        if (book['name'].lower() == book_name_lookup.lower()):
+            book_found = True
+            print(f"{book['name'].title()} has been deleted from the directory.");
+        else:
+            updated_data.append(book);
+    if book_found:
+        with open('utils/books.json', 'w') as file:
+            json.dump(updated_data, file, indent=4);
+    else:
+        print(f"{book_name_lookup} is not in directory"); 
+
 
 def menu():
-    user_input = input(USER_CHOICE);
-    while user_input != 'q':
+    while True:
+        user_input = input(USER_CHOICE);
         if user_input.lower() == 'a':
             prompt_add_book();
         elif user_input.lower() == 'l':
             list_books();
         elif user_input.lower() == 'r':
-            prompt_add_book();
+            prompt_read_book();
         elif user_input.lower() == 'd':
             prompt_delete_book();
-        else:
+        elif user_input.lower() == 'q':
+            print("Process terminated.");
             break;
-    print("Process terminated.");
+        else:
+            print("Invalid choice.\nPlease choose a valid option.");
 
 menu();
--- utils/database.py
+++ utils/database.py
@@ -2,6 +2,26 @@ import json;
 
 books = [];
 
-def add_book():
+def call_books(list_arr):
+    print("Generating list of books in directory...................................................................")
+    for book in books:
+        print(f"\nBook Title: {book['name'].title()}\n{book['name'].title()} Author: {book['author'].title()}\n{book['name'].title()} Read?: {book['read']}\n");
+
+def add_book(list_arr):
+    filename = 'utils/books.json';
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file);
+            for book in data:
+                list_arr.append(book);
+        print("Generating list of books in directory...................................................................")
+        for book in list_arr:
+            print(f"\nBook Title: {book['name'].title()}\n{book['name'].title()} Author: {book['author'].title()}\n{book['name'].title()} Read?: {book['read']}\n"); 
+    except FileNotFoundError:
+        print(f"File in directory is empty, improperly formatted, or corrupted");
+
+""" def add_book():
     with open('books.json', 'a') as file:
-        json.dump(books, file);
+        json.dump(books, file);"""
+
+add_book();
